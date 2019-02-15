07700092b9934e55259d55f9721068b66a61096c
Supratim <supratim.gre2014@yahoo.com>
2019-02-15 09:54:37 +0100

    Second Commit

---
+++ src/main/scala/SparkDataframeTutorials/DataframeBasics.scala
@@ -0,0 +1,27 @@
+package SparkDataframeTutorials
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j._
+
+object DataframeBasics extends java.io.Serializable {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession.builder
+                            .appName("SparkSQL")
+                            .master("local[*]")
+                            .getOrCreate()
+    val df = spark.read.option("header","true").option("InferSchema","true").csv("../MLwithSpark/Data/CitiGroup2006_2008")
+    // print the dataframe
+    //df.show(5)
+    // show summary of the dataframe
+    //df.describe().show()
+    // show only one column
+    //df.select("Date", "Volume").show(5)
+    // create a new column based on existing column
+    val df2 = df.withColumn("HighPlusLow", df("High")+df("Low"))
+    df2.select("HighPlusLow", "High", "Low").show(5)
+    // check schema
+    //df2.printSchema()
+    // alias
+    df2.select(df2("HighPlusLow").as("HPL")).show(5)
+  }
+}
